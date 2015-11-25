14fc61da238d1ffd74cdc8693ffc04c2d6427842
Sam Osborn <capttrousers@users.noreply.github.com>
2015-11-24 21:45:49 -0600

    Update x10hosting-db-insertData.sql

--- x10hosting-db-insertData.sql
+++ x10hosting-db-insertData.sql
@@ -1,7 +1,7 @@
 USE skipduck_campaign;
 
 INSERT INTO mail VALUES 
-	('Yes', DEFAULT, 'Sam', NULL, 'Osborn', NULL, 'NYC', 'NY', 10111, 'testing@test.com', 8609335656, '1988-03-18'),
+	('Yes', DEFAULT, 'Sam', NULL, 'Osborn', NULL, 'NYC', 'NY', 10111, 'testing@test.com', 8009995555, '1984-03-08'),
     ('No', DEFAULT, 'Fernanda', NULL, 'Hennessey', NULL, 'Washington', 'DC', 20456, 'notareal@emailtest.test', 5551234567, '1990-12-10'),
     ('Yes', DEFAULT, 'William', NULL, 'Hennessey', NULL, 'NYC', 'NY', 10200, 'testagain@test.again', 9229884321, '1985-05-29'),
     ('Yes', DEFAULT, 'Hilary', NULL, 'Osborn', NULL, 'Brooklyn', 'NY', 14302, 'hilarytest@email.test', 8605555555, '1988-08-17');
